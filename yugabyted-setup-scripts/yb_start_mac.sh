d4af5e1c1fa801d3444e41fe8b874726f8358036
James Knicely <jknicely@jk-mbp-4md6q.tail5955.ts.net>
2023-09-26 16:11:50 -0400

    Fixed yb start script and QCon logo

--- yugabyted-setup-scripts/yb_start_mac.sh
+++ yugabyted-setup-scripts/yb_start_mac.sh
@@ -3,19 +3,19 @@
 
 # Start primary node
 printf "\nStarting primary node...\n"
-$PWD/yugabyte-2.19.2.0/bin/yugabyted start --advertise_address 127.0.0.1 --base_dir=$PWD/node1 --cloud_location onprem.dc1.az1 --fault_tolerance=zone --tserver_flags="ysql_enable_packed_row=true,ysql_beta_features=true,stream_compression_algo=3,yb_enable_read_committed_isolation=true,enable_deadlock_detection=true,enable_wait_queues=true" --master_flags="ysql_enable_packed_row=true,ysql_beta_features=true,transaction_tables_use_preferred_zones=true"
+$PWD/yugabyte-2.19.2.0/bin/yugabyted start --advertise_address 127.0.0.1 --base_dir=$PWD/node1 --cloud_location cloud.region.az1 --fault_tolerance=zone --tserver_flags="ysql_enable_packed_row=true,ysql_beta_features=true,stream_compression_algo=3,yb_enable_read_committed_isolation=true,enable_deadlock_detection=true,enable_wait_queues=true" --master_flags="ysql_enable_packed_row=true,ysql_beta_features=true,transaction_tables_use_preferred_zones=true"
 
 until $PWD/yugabyte-2.19.2.0/postgres/bin/pg_isready -h 127.0.0.1 -p 5433 ; do sleep 1 ; done
 
 # Start 2nd node
 printf "\nStarting node 2...\n"
-$PWD/yugabyte-2.19.2.0/bin/yugabyted start --advertise_address 127.0.0.2 --base_dir=$PWD/node2 --cloud_location onprem.dc1.az2 --fault_tolerance=zone --join=127.0.0.1 --tserver_flags="ysql_enable_packed_row=true,ysql_beta_features=true,stream_compression_algo=3,yb_enable_read_committed_isolation=true,enable_deadlock_detection=true,enable_wait_queues=true" --master_flags="ysql_enable_packed_row=true,ysql_beta_features=true,transaction_tables_use_preferred_zones=true"
+$PWD/yugabyte-2.19.2.0/bin/yugabyted start --advertise_address 127.0.0.2 --base_dir=$PWD/node2 --cloud_location cloud.region.az2 --fault_tolerance=zone --join=127.0.0.1 --tserver_flags="ysql_enable_packed_row=true,ysql_beta_features=true,stream_compression_algo=3,yb_enable_read_committed_isolation=true,enable_deadlock_detection=true,enable_wait_queues=true" --master_flags="ysql_enable_packed_row=true,ysql_beta_features=true,transaction_tables_use_preferred_zones=true"
 
 until $PWD/yugabyte-2.19.2.0/postgres/bin/pg_isready -h 127.0.0.2 -p 5433 ; do sleep 1 ; done
 
 # Start 3rd node
 printf "\nStarting node 3...\n"
-$PWD/yugabyte-2.19.2.0/bin/yugabyted start --advertise_address 127.0.0.3 --base_dir=$PWD/node3 --cloud_location onprem.dc1.az3 --fault_tolerance=zone --join=127.0.0.1 --tserver_flags="ysql_enable_packed_row=true,ysql_beta_features=true,stream_compression_algo=3,yb_enable_read_committed_isolation=true,enable_deadlock_detection=true,enable_wait_queues=true" --master_flags="ysql_enable_packed_row=true,ysql_beta_features=true,transaction_tables_use_preferred_zones=true"
+$PWD/yugabyte-2.19.2.0/bin/yugabyted start --advertise_address 127.0.0.3 --base_dir=$PWD/node3 --cloud_location cloud.region.az3 --fault_tolerance=zone --join=127.0.0.1 --tserver_flags="ysql_enable_packed_row=true,ysql_beta_features=true,stream_compression_algo=3,yb_enable_read_committed_isolation=true,enable_deadlock_detection=true,enable_wait_queues=true" --master_flags="ysql_enable_packed_row=true,ysql_beta_features=true,transaction_tables_use_preferred_zones=true"
 
 until $PWD/yugabyte-2.19.2.0/postgres/bin/pg_isready -h 127.0.0.3 -p 5433 ; do sleep 1 ; done
 
