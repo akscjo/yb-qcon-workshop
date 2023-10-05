a974ed99c1b37d290937278eed3b1d5e7f42e0df
Amit Chauhan <amchauhan@yugabyte.com>
2023-10-04 23:08:39 -0700

    some minor changes

--- workload-service/src/main/java/com/yugabyte/simulation/config/DatasourceConfig.java
+++ workload-service/src/main/java/com/yugabyte/simulation/config/DatasourceConfig.java
@@ -11,37 +11,17 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DatasourceConfig {
-//	@Bean
-//	@ConfigurationProperties("app.datasource")
-//	public HikariDataSource dataSource() {
-//		DataSourceProperties props  = new DataSourceProperties();
-//	    return (HikariDataSource) DataSourceBuilder.create()
-//	            .type(HikariDataSource.class)
-//	            .build();
-//	}
-	
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.hikari")
     public HikariConfig hikariConfig() {
-        //return new HikariConfig();
     	HikariConfig config = new HikariConfig();
-//    	Properties properties = new Properties();
-//    	properties.setProperty(PGProperty.PREFER_QUERY_MODE.getName(), PreferQueryMode.SIMPLE.value());
-//    	config.setDataSourceProperties(properties);
-    	
-//    	Properties props = config.getDataSourceProperties();
-    	//props.setProperty(PGProperty.PREFER_QUERY_MODE.getName(), PreferQueryMode.EXTENDED_CACHE_EVERYTHING.value());
-//    	config.setDataSourceProperties(props);
     	return config;
     }
 
     @Bean
     @Primary // may not be required
     public DataSource dataSource() {
-//    	Properties properties = 
     	HikariDataSource ds = new HikariDataSource(hikariConfig());
-//    	ds.addDataSourceProperty(PGProperty.PREFER_QUERY_MODE.getName(), PreferQueryMode.EXTENDED_CACHE_EVERYTHING);
-////        return new HikariDataSource(hikariConfig());
     	return ds;
     }
 }
